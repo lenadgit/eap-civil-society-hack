22da200d0ca89ffa947cbac69b3e5b4b520dcdb8
Javid <31157303+javid020@users.noreply.github.com>
2019-04-17 17:04:32 +0400

    added some funcs

--- app/Http/Helper.php
+++ app/Http/Helper.php
@@ -38,6 +38,27 @@ if (!function_exists('getCurrentUrl')) {
 
 }
 
+if (! function_exists('reading_time')) {
+    function reading_time($text)
+    {
+        if (! $text) {
+            return null;
+        }
+        $words = str_word_count(strip_tags($text));
+        $minutes = floor($words / 120);
+        $seconds = floor($words % 120 / (120 / 60));
+
+        if (1 <= $minutes) {
+            $estimated_time = $minutes . ' min' . ($minutes == 1 ? '' : 's');
+            //. ', ' . $seconds . ' sec' . ($seconds == 1 ? '' : 's')
+        } else {
+            $estimated_time = $seconds . ' sec' . ($seconds == 1 ? '' : 's');
+        }
+
+        return $estimated_time;
+    }
+}
+
 
 
 
