35e87acb2dab33b465900a2b36d8761f35f2d7b1
Lena <lena.boruslav@gmail.com>
2020-07-25 13:24:56 +0300

    new map added

---
+++ js/novyirozdil-coordinates.js
@@ -0,0 +1,101 @@
+var roads = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.133137,
+                    49.470820
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Послуги з відновлення дорожнього покриття після розкопки<br><br><b>14 873 грн</b><br><br>"
+            },
+            "id": 100
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.138917,
+                    49.469650
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Поточний ремонт дороги по вулиці B.Чорновола <br>(відрізок дороги від вулиці ходорівська в напрямку вулиці о.довбуша)<br><br><b>135000 грн</b>"
+            },
+            "id": 101
+        },       
+    ]
+};
+
+var buildings = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.1433092,
+                    49.4684506
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Капітальний ремонт прибудинкової території по вул. Cтепана Бандери<br><br><b>380 345 грн</b><br><br>"
+            },
+            "id": 100
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.148199,
+                    49.4681685
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Капітальний ремонт внутрішньобудинкових інженерних мереж<br> водопровідної мережі житлового будинку №40<br><br><b>50 391 грн</b>"
+            },
+            "id": 101
+        },       
+    ]
+};
+
+var schools = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.149114,
+                    49.467683
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Капітальний ремонт приміщення пральні, <br>гладильної та кухонного блоку ДНЗ Малятко<br><br><b>356 989 грн</b><br><br>"
+            },
+            "id": 100
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.138497,
+                    49.470577
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Капітальний ремонт. <br>Заміна підлоги в коридорі першого поверху. <br>Заміна вікон на металопластикові - 3 шт. другого поверху ДНЗ № 5 Голубок<br><br><br><b>50 997 грн</b>"
+            },
+            "id": 101
+        },       
+    ]
+};
